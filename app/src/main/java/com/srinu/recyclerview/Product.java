a40e3a918e57dadcaa255dce952ecfda5301c2db
srinivasarao <srinivasaraouesi@gmail.com>
2017-12-27 19:23:08 +0530

    first commit

---
+++ app/src/main/java/com/srinu/recyclerview/Product.java
@@ -0,0 +1,47 @@
+package com.srinu.recyclerview;
+
+/**
+ * Created by ConquerorsTechnologies on 14/12/17.
+ */
+
+public class Product {
+    private int id;
+    private String title;
+    private String shortdesc;
+    private double rating;
+    private double price;
+    private int image;
+
+    public Product(int id, String title, String shortdesc, double rating, double price, int image) {
+        this.id = id;
+        this.title = title;
+        this.shortdesc = shortdesc;
+        this.rating = rating;
+        this.price = price;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getShortdesc() {
+        return shortdesc;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getImage() {
+        return image;
+    }
+}
